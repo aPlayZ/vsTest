fb7180480ad92241699c7bcb2150eedd0d948b55
aPlayZ <play_z@aliyun.com>
2017-01-21 18:01:53 +0800

    hello,word,2

---
+++ vsGitTest-ConsoleApplication1/vsGitTest-ConsoleApplication1/vsGitTest-ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// vsGitTest-ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello, world" << endl;
+	cout << "s" << endl;
+    return 0;
+}
+
